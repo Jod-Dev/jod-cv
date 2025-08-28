33d1a19d374a61c3d0af6e034b69569694935a9b
IYFOrlando <orlando@iyfusa.org>
2025-08-28 09:19:26 -0400

    Update build files for Cloudflare deployment

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"cQ7im7Wm1jm29DQ0/8CV2jmZgFyuDU0+ec3EzSXRzVU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Xo6gbWzCfN0FDUtcl0yXxHTqQ7OIMDdWVWcymO1MlNQ=\"}"
