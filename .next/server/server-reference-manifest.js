9e24c9fc2f6246a77ec03bab0e2f2e5ff6f4815f
IYFOrlando <orlando@iyfusa.org>
2025-08-28 10:17:36 -0400

    Fix Cloudflare deployment - clean build and proper static files

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LY6y83klOKN/YsS4hHJ1TGCOdwH5ytEuAzN4w0eYJCI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"viH4PZL9IVQKaEFEn0y9cvL4f2yyssRtgWd+My5D+gI=\"}"
